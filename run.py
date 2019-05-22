58549150b4e8ccfa8abfd6ef4da3f1b3e95d73b8
Yujin <yujinjcho@gmail.com>
2019-05-21 21:40:21 -0700

    procfile and no https

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ from application import app
 
 if __name__ == '__main__':
     app.run(
-        port=7000,
-        ssl_context=('cert.pem', 'key.pem')
+        port=7000
+        # ssl_context=('cert.pem', 'key.pem')
     )
