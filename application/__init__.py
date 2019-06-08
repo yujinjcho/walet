3fc0d5a77a1368416516de7981eb6486a1a7cf48
Yujin <yujinjcho@gmail.com>
2019-06-08 10:12:42 -0700

    add application environ

--- application/__init__.py
+++ application/__init__.py
@@ -1,9 +1,19 @@
 import os
 from flask import Flask
+from flask_cors import CORS
+
+from config import APPLICATION_ENVIRONMENT
 
 
 def create_app():
-    app = Flask(__name__, static_folder='build/static', template_folder='build')
+    if APPLICATION_ENVIRONMENT == 'production':
+        flask_options = {'static_folder':'build/static', 'template_folder':'build'}
+    else:
+        flask_options = {}
+
+    app = Flask(__name__, **flask_options)
+
+    CORS(app)
     app.config.from_object('config')
 
     try:
