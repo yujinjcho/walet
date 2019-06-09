17b1ea42db6f31978d03b098d2dabc0a9b4fa9c0
Yujin <yujinjcho@gmail.com>
2019-06-08 23:15:36 -0700

    use react hook for Login

--- front/src/Login.js
+++ front/src/Login.js
@@ -1,41 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 
 import api from './api';
 
 import './Login.css';
 
-class Login extends Component {
+function Login() {
+  const [authRedirectUrl, setAuthRedirectUrl] = useState(undefined);
 
-  state = {
-    authRedirectUrl: undefined
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await api.fetchHelper('/api/auth/login')
+        .then(res => res.json())
+        .then(res => res.result)
+        .catch(_ => {
+          console.error('failed to retrieve authentication url');
+          return undefined;
+        })
 
-  componentDidMount() {
-    api.fetchHelper('/api/auth/login')
-      .then(res => res.json())
-      .then(res => this.setState({ authRedirectUrl: res.result }))
-      .catch(_ => console.error('failed to retrieve authentication url'))
-  }
+      setAuthRedirectUrl(result);
+    };
 
-  render() {
-    const { authRedirectUrl } = this.state;
+    fetchData();
+  }, []);
 
-    return (
-      <div className='login-section'>
-        <div className='login-logo'>
-          Walet
-        </div>
-          { authRedirectUrl &&
-            <a href={ authRedirectUrl }>
-              <Button variant="primary" size="lg" block >
-                Login
-              </Button>
-            </a>
-          }
+  return (
+    <div className='login-section'>
+      <div className='login-logo'>
+        Walet
       </div>
-    );
-  }
+        { authRedirectUrl &&
+          <a href={ authRedirectUrl }>
+            <Button variant="primary" size="lg" block >
+              Login
+            </Button>
+          </a>
+        }
+    </div>
+  );
 }
 
 export default Login;
