ffeb958da3e9ef3de9e731900397ab4137c1df15
Yujin <yujinjcho@gmail.com>
2019-07-19 09:04:34 -0700

    update webhook url

--- heroku_deploy.sh
+++ heroku_deploy.sh
@@ -6,7 +6,7 @@ sed \$d .gitignore > .gitignore_temp
 mv .gitignore_temp .gitignore
 
 cd front
-REACT_APP_WEBHOOK_URL=https://g2sfivq5i5.execute-api.us-west-2.amazonaws.com/prod npm run build
+REACT_APP_WEBHOOK_URL=https://g2sfivq5i5.execute-api.us-west-2.amazonaws.com/prod/v1/webhook npm run build
 cd ..
 mv front/build application/
 
